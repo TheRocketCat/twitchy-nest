b184e739367a8d80dba33568625800b72680fbd3
RocketCat <dakondo@protonmail.com>
2021-03-21 03:51:33 +0100

    nice easy decorator to require twitch auth

--- src/twitch-bot/auth.ts
+++ src/twitch-bot/auth.ts
@@ -1,70 +1,8 @@
 import { Userstate } from 'tmi.js';
 import { Result, Err } from 'ts-results';
+import "reflect-metadata";
 
-//TODO dubble check this is true, but seems like it or maybe username.lower
-export function isOwner(channel: string, userstate: Userstate): boolean {
-	//TODO this assumes that channel is always lower case
-	return channel == '#' + userstate.username.toLowerCase();
-}
-
-/**
- * Functions using this decorator should return Result,
- * because atm the type system isnt enforcing that properly.
- * Also it must take at least two parameters in this order
- * @param {string} channel
- * @param {Userstate} userstate
- */
-export function TwitchOwner() {
-	return function (
-		target: any,
-		propertyKey: string,
-		descriptor: PropertyDescriptor,
-	) {
-		const OGFunc = descriptor.value;
-
-		//temporary annoyance, since idk how to make type system check this for me
-		const isResult = function (r: any): r is Result<any, any> {
-			if (r == undefined) {
-				throw new Error(
-					'Read the fucking manual: your function must return a result',
-				);
-			} else if (!r.hasOwnProperty('ok') || !r.hasOwnProperty('err')) {
-				throw new Error(
-					'Read the fucking manual: your function must return a result',
-				);
-			}
-			return true;
-		};
-
-		descriptor.value = function (
-			channel: string,
-			userstate: Userstate,
-			...args: any[]
-		): Result<any, Error> {
-			//typesystem doesnt catch incorrect usage of the decorator
-			//so here is some helpfull error messages for yall who didnt RTFM
-			if (channel == undefined || typeof channel != 'string') {
-				throw new Error(
-					'Read the fucking manual: your functions first parameter has to be a channel',
-				);
-			} else if (userstate == undefined) {
-				throw new Error(
-					'Read the fucking manual: your functions second parameter has to be a Userstate',
-				);
-			}
-
-			if (isOwner(channel, userstate)) {
-				const hopefullyAResult = OGFunc(channel, userstate, ...args);
-				//temporary annoyance, since idk how to make type system check this for me
-				if (isResult(hopefullyAResult)) {
-					return hopefullyAResult;
-				}
-			} else {
-				return Err(new UnauthorizedError());
-			}
-		};
-	};
-}
+const requiredMetadataKey = Symbol("required");
 
 //ERRORS
 export class UnauthorizedError extends Error {
@@ -72,5 +10,53 @@ export class UnauthorizedError extends Error {
 		super();
 		this.name = 'unauthorized error';
 		this.message = 'user is not authorized';
+	}
+}
+
+//TODO dubble check this is true, but seems like it or maybe username.lower
+export function isOwner(channel: string, userstate: Userstate): boolean {
+	//TODO this assumes that channel is always lower case
+	return channel == '#' + userstate.username.toLowerCase();
+}
+
+//TODO investigate using linting to properly tell users of this decorators 
+// requirments
+/**
+ * @param {string} channel
+ * @param {Userstate} userstate
+ * A decorating that makes sure only the owner of the cannel can
+ * call this function.
+ * The decorated method must take at least channel:string and userstate:Userstate
+ * arguments to function.
+ * Functions using this decorator must return Result.
+ */
+export function TwitchOwner(){
+	return function (
+		target: any,
+		propertyName: string,
+		descriptor: PropertyDescriptor,
+	) {
+		  let method = descriptor.value!;
+
+		  descriptor.value = async function ():Promise<Result<any,Error>> {
+			let requiredParameters: any[] = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyName);
+			if (requiredParameters) {
+			  for (let parameterIndex of requiredParameters) {
+				if (parameterIndex >= arguments.length || arguments[parameterIndex] === undefined) {
+				  throw new Error("Missing required argument.");
+				}
+			  }
+			}
+
+			const [channel,userstate] = arguments
+
+			if(channel == "#" + userstate.username.toLowerCase()){
+				const res=await method.apply(this,arguments)
+				if(res.ok == undefined || res.err == undefined){
+					throw new Error("method must return type Result< your type ,Error>")
+				}
+				return res
+			}else return Err(new UnauthorizedError())
+		  };
 	}
 }
