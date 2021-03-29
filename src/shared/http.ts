95eb694591c06d5ec02e9c931a1b816b1d45b836
RocketCat <dakondo@protonmail.com>
2021-03-29 17:25:38 +0200

    cleaner way to handle http requests

--- src/shared/http.ts
+++ src/shared/http.ts
@@ -1,4 +1,5 @@
 import { Result ,Ok ,Err } from "ts-results"
+import fetch, {Response} from "node-fetch"
 
 export async function fetchJson(url:string):Promise<Result<any,Error>>{
     try{
@@ -10,8 +11,22 @@ export async function fetchJson(url:string):Promise<Result<any,Error>>{
     }
 }
 
+export async function fetchJsonProperty(url:string,prop:string){
+    try{
+        const res=await fetch(url)
+        correctStatus(res).expect("check status")
+		const json=await res.json()
+		if(json[prop]==undefined){
+			return Err(new Error("property is undefined"))
+		}
+        return Ok(json[prop])
+    }catch(e){
+        return Err(e)
+    }
+}
+
 export function correctStatus(r:Response):Result<void,Error>{
-    if(r.status <= 200 && r.status > 300){
+    if(r.status < 200 || r.status > 299){
         return Err(new Error(`code ${r.status}`))
     }
     return Ok.EMPTY
