ba0d58e6e218a5ad1d8446057a3bfb98693a355f
RocketCat <dakondo@protonmail.com>
2021-03-29 17:27:34 +0200

    new way to execute cmds

--- src/twitch-bot/cmd-handler.ts
+++ src/twitch-bot/cmd-handler.ts
@@ -6,25 +6,76 @@ import { extractCommandArgs } from './shared/utilities/args';
 import { TwitchInfoCommand } from './info-command/info-command';
 import { TwitchRallyCommand } from "./rally/commands";
 import {UserError} from "../shared/error"
+import {IToString} from "./shared/interfaces/string"
 
 import {InfoCommandService} from "../info-command/info-command.service"
 
 export class CmdHandler {
+	private cmds:Map<string,any>
 	constructor(
 		private infoCommand: TwitchInfoCommand,
 		private rallyCommand: TwitchRallyCommand
-	) {}
+	) {
+		this.cmds=new Map()
+		this.mount(rallyCommand)
+		this.mount(infoCommand)
+	}
+
+	private mount(c:any){
+		const prototype=Object.getPrototypeOf(c)
+		let b=prototype.constructor==undefined
+		if(b){
+			//means that it was called on mock 
+			//most likely and therefor should be skipped
+			return
+		}
+		b=prototype.constructor.scmds==undefined
+		if(b){
+			//means that it was called on mock 
+			//most likely and therefor should be skipped
+			return
+		}
+		//console.log(prototype)
+		prototype.constructor.scmds.forEach(([cmd,prop])=>{
+			const f=c[prop]
+			const boundF=f.bind(c)
+			this.cmds.set(cmd,boundF)
+		})
+	}
+
+	async cmdSwitch(channel:string,userstate:Userstate,msg:string,self:boolean){
+		const cmd = msg.trim().split(' ')[0].substring(1);
+
+		const f=this.cmds.get(cmd)
+		if(f==undefined){
+			//check custom commands
+			return await this.infoCommand.get(channel,cmd)
+			//return Err(new UserError("no such command"))
+		}
+
+		let args:any[];
+		try{
+			args = extractCommandArgs(msg).expect('parsing args');
+		}catch(e){
+			return Err(new UserError("double check your cmd arguments"))
+		}
+
+		let cmdResult: Result<IToString|string|void, Error>;
+
+		const res=await f(channel,userstate,args)
+		return res
+	}
 
 	async executeCmd(
 		channel: string,
 		userstate: Userstate,
 		msg: string,
 		self: boolean,
-	): Promise<Result<any, Error>> {
+	): Promise<Result<IToString|string|void, Error>> {
 		//remove extra whitesplace, get first cmd and remove cmd symbol
 		const cmd = msg.trim().split(' ')[0].substring(1);
 
-		let cmdResult: Result<any, Error>;
+		let cmdResult: Result<IToString|string|void, Error>;
 		let args:any[];
 		try{
 			args = extractCommandArgs(msg).expect('parsing args');
@@ -33,26 +84,9 @@ export class CmdHandler {
 		}
 		//standard commands
 		switch (cmd) {
-			/*
-			case "!createAutoMsg":
-				cmdResult=await createNewAutoMsgHandler(channel,userstate,msg);
-			break
-			case "!coinCount":
-				cmdResult=await getCoinCountHandler(msg)
-			break
-			case "!coinPrice":
-				cmdResult=await getCoinPriceHandler(msg)
-			break
-			case "!balance":
-				cmdResult=await getBalanceHandler(msg)
-			break
-			case "!volume":
-				cmdResult=await getVolumeHandler(msg)
-			break
-			*/
 		   //Rally
 		   case TwitchRallyCommand.getCoinCountCmd:
-				cmdResult=await this.rallyCommand.getCoinCount(args)
+				cmdResult=await this.rallyCommand.getCoinCount(channel,userstate,args)
 				break
 
 			//InfoCommand 
