30661c36b4b16cfba6cf86bd707ce441d6947131
RocketCat <dakondo@protonmail.com>
2021-03-31 21:33:34 +0200

    add auto command

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InfoCommandModule } from './info-command/info-command.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AutoCommandService } from './auto-command/auto-command.service';
+import { AutoCommandModule } from './auto-command/auto-command.module';
 
 @Module({
 	imports: [
 		InfoCommandModule,
 		MongooseModule.forRoot('mongodb://localhost/twitchy'),
+		AutoCommandModule,
 	],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService, AutoCommandService],
 	//,exports:[InfoCommandModule]
 })
 export class AppModule {}
